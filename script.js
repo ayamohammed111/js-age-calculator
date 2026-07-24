5c800cba0cb2a58e39faf5b952ee177a9a3a8bfc
Aya <ayamohammed2392007@gmail.com>
2026-07-24 22:29:20 +0300

    My first JavaScript project

---
+++ script.js
@@ -0,0 +1,8 @@
+const firstName ='Aya';
+const job ='programmer';
+const currentYear = 2026;
+const birthYear = 2007 ;
+const Aya = "I'm "+ firstName +' ,a '+(currentYear - birthYear)+' years old '+job+'! '
+console.log(Aya);
+const AyaNew =`I'm ${firstName} ,a ${currentYear - birthYear} years old ${job}!`;
+console.log(AyaNew );
